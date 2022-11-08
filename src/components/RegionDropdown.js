9e2cdd35925b1e9e2aff0145bd8791e256d7c063
vergarapog <201801166@iacademy.edu.ph>
2022-11-08 18:41:23 +0800

    finished fe for searchbar and region dropdown

--- src/components/RegionDropdown.js
+++ src/components/RegionDropdown.js
@@ -1,16 +1,28 @@
 import React from "react"
+import { useGlobalContext } from "../context"
 
 const RegionDropdown = () => {
+  const { isDarkMode } = useGlobalContext()
   return (
-    <div className="inline-block relative w-64">
-      <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-        <option>Really long option that will likely overlap the chevron</option>
-        <option>Option 2</option>
-        <option>Option 3</option>
+    <div className="relative w-52 text-lighttext dark:text-darktext">
+      <select className="appearance-none w-full bg-lightbg dark:bg-darkelement px-4 py-3 pr-8 rounded shadow leading-tight focus:outline-none">
+        <option value="" disabled selected hidden>
+          Filter by Region
+        </option>
+        <option>All</option>
+        <option>Africa</option>
+        <option>America</option>
+        <option>Asia</option>
+        <option>Europe</option>
+        <option>Oceania</option>
       </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <div
+        className={`pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 ${
+          isDarkMode ? "text-darktext" : "text-lighttext"
+        }`}
+      >
         <svg
-          className="fill-current h-4 w-4"
+          class="fill-current h-4 w-4"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
         >
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -7,14 +7,14 @@ const Searchbar = () => {
   const { isDarkMode } = useGlobalContext()
 
   return (
-    <div className="min-w flex dark:bg-darkelement px-4 rounded-lg space-x-2">
+    <div className="min-w flex bg-lightelement shadow dark:bg-darkelement px-4 rounded-lg space-x-2">
       <MagnifyingGlass
         className="inline my-auto"
         size={"22px"}
         color={isDarkMode ? "#ffffff" : "#000000"}
       />
       <input
-        className="text-lighttext dark:bg-darkelement shadow appearance-none  w-full py-3 px-3 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+        className="text-lighttext dark:bg-darkelement appearance-none  w-full py-3 px-3 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
         id="username"
         type="text"
         placeholder="Search for a country..."
