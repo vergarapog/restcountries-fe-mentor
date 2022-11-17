91075a3fd84c259cb8cb5bd6a0fd5b50a7a8644f
vergarapog <201801166@iacademy.edu.ph>
2022-11-17 16:51:05 +0800

    fixed flag img width and height

--- src/components/CountryListPaginated.js
+++ src/components/CountryListPaginated.js
@@ -59,12 +59,12 @@ const Countries = ({ countries }) => {
 const Country = ({ country }) => {
   const { name, population, region, capital, flags } = country
   return (
-    <div className="w-64 h-80 bg-lightelement dark:bg-darkelement text-lighttext dark:text-darktext">
+    <div className="grid grid-rows-2 w-64 h-80 bg-lightelement dark:bg-darkelement text-lighttext dark:text-darktext">
       {" "}
-      <div>
-        <img src={flags.png} alt="" />
+      <div className="">
+        <img src={flags.png} className="w-full h-full" alt="" />
       </div>{" "}
-      <div className="p-4 text-lighttext dark:text-darktext">
+      <div className=" p-4 text-lighttext dark:text-darktext">
         <h2 className="font-extrabold text-lg">{name.common}</h2>
         <div className="pt-4 text-sm">
           <p className="font-light">
