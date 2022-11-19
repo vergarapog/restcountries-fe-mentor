b5bc46ea65142b4c28786d06aee510ad265a4621
vergarapog <201801166@iacademy.edu.ph>
2022-11-19 21:19:50 +0800

    implemented light mode color for loading circle

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,13 @@
 import ClipLoader from "react-spinners/ClipLoader"
+import { useGlobalContext } from "../context"
 
 const Loading = () => {
+  const { isDarkMode } = useGlobalContext()
+
   return (
     <div className="flex justify-center">
       <ClipLoader
-        color="#ffffff"
+        color={`${isDarkMode ? "#ffffff" : "#202c37"}`}
         loading={true}
         size={75}
         aria-label="Loading Spinner"
