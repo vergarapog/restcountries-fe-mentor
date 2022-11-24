c8e919f70dcef61ecc8e9e0897e1b4de6aab02b7
vergarapog <201801166@iacademy.edu.ph>
2022-11-24 22:15:59 +0800

    single country page 85% completed

--- src/Pages/SingleCountry.js
+++ src/Pages/SingleCountry.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom"
 import { ArrowLeft } from "phosphor-react"
 
 const SingleCountry = ({ country }) => {
-  const { name, population, region, capital, flags } = country
+  if (!country) {
+    return <div>Loading....</div>
+  }
   return (
     <section>
       <div>
@@ -14,29 +16,61 @@ const SingleCountry = ({ country }) => {
         </Link>
       </div>
       <div className="bg-lightelement dark:bg-darkelement text-lighttext dark:text-darktext">
-        <img src={flags.png} className="w-full h-full" alt="" />
+        <img src={country.flags.png} className="w-full h-full" alt="" />
       </div>{" "}
-      <div className=" p-4 text-lighttext dark:text-darktext">
-        <h2 className="font-extrabold text-lg">{name.common}</h2>
-        <div className="pt-4 text-sm">
-          <p className="font-light">
+      <div className=" p-4  text-lighttext dark:text-darktext">
+        <h2 className="font-extrabold text-xl">{country.name.common}</h2>
+        <div className="pt-4 text-sm space-y-3">
+          <div className="font-light flex space-x-2">
             <span className="font-semibold">Native name:</span>
             <ul>
               {" "}
-              {Object.values(name.nativeName).map((name) => {
-                return <li>{name.common}</li>
+              {Object.values(country.name.nativeName).map((name) => {
+                return <li key={name.common}>- {name.common}</li>
               })}
             </ul>
+          </div>
+          <p className="font-light">
+            <span className="font-semibold">Population:</span>{" "}
+            {country.population.toLocaleString()}
           </p>
           <p className="font-light">
-            <span className="font-semibold">Population:</span> {population}
+            <span className="font-semibold">Region:</span> {country.region}
           </p>
           <p className="font-light">
-            <span className="font-semibold">Region:</span> {region}
+            <span className="font-semibold">Sub Region:</span>{" "}
+            {country.subregion}
           </p>
           <p className="font-light">
-            <span className="font-semibold">Capital:</span> {capital}
+            <span className="font-semibold">Capital:</span> {country.capital}
           </p>
+        </div>
+
+        <div className="pt-12 text-sm space-y-3">
+          <p className="font-light">
+            <span className="font-semibold">Top Level Domain:</span>{" "}
+            {country.tld}
+          </p>
+          <p className="font-light">
+            <span className="font-semibold">Currencies:</span>{" "}
+            {Object.values(country.currencies).map((i) => (
+              <>{i.name}</>
+            ))}
+          </p>
+          <p className="font-light">
+            <span className="font-semibold">Languages:</span>{" "}
+            {Object.values(country.languages).map((language, index) => {
+              if (index < Object.values(country.languages).length - 1) {
+                return <>{language}, </>
+              } else {
+                return <>{language}</>
+              }
+            })}
+          </p>
+        </div>
+
+        <div className="pt-12 space-y-3">
+          <h2>Border Countries:</h2>
         </div>
       </div>
     </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
   return (
     <section>
-      <div className="bg-lightbg dark:bg-darkbg text-lighttext dark:text-darktext text-center pt-40 pb-6">
+      <div className="bg-lightbg dark:bg-darkbg text-lighttext dark:text-darktext text-center pt-40 pb-6 transition duration-300">
         Made with love by Brian Vergara. Inspired by FrontEndMentor
       </div>
     </section>
