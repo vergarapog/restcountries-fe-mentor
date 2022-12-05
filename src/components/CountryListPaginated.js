c435e3cc159e2080f1b168cac6197384b6e87504
vergarapog <201801166@iacademy.edu.ph>
2022-12-05 18:16:31 +0800

    added: tests to searchbar component and singlecountry component

--- src/components/CountryListPaginated.js
+++ src/components/CountryListPaginated.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import ReactPaginate from "react-paginate"
 import { useSelector } from "react-redux"
 import Loading from "./Loading"
---
+++ src/components/Searchbar.test.js
@@ -0,0 +1,52 @@
+import React from "react"
+import "@testing-library/jest-dom/extend-expect"
+import { getAllByText, render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import Searchbar from "./Searchbar"
+
+import { AppProvider } from "../context"
+import { Provider } from "react-redux"
+
+import store from "../reducers/store"
+import { MemoryRouter as Router } from "react-router-dom"
+
+test("renders searchbar correctly with placeholder text", async () => {
+  render(
+    <Router>
+      <Provider store={store}>
+        <AppProvider>
+          <Searchbar />
+        </AppProvider>
+      </Provider>
+    </Router>
+  )
+
+  const placeholder = await screen.findByPlaceholderText(
+    "Search for a country..."
+  )
+
+  expect(placeholder).toBeDefined()
+})
+
+test("searchField gets values when the user types", async () => {
+  render(
+    <Router>
+      <Provider store={store}>
+        <AppProvider>
+          <Searchbar />
+        </AppProvider>
+      </Provider>
+    </Router>
+  )
+
+  const testUser = userEvent.setup()
+  const searchField = await screen.findByPlaceholderText(
+    "Search for a country..."
+  )
+
+  await testUser.type(searchField, "phili")
+
+  const textByUser = searchField.value
+
+  expect(textByUser).toBe("phili")
+})
